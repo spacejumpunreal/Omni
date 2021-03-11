5a417b2db685e3322fd1b821c39fa75cdea00743
lxdeng <lxdeng@tencent.com>
2021-03-11 19:57:01 +0800

    add lockfreestack experiment

--- Engine/Source/Programs/PlayGround/PlayGroundExperiment.cpp
+++ Engine/Source/Programs/PlayGround/PlayGroundExperiment.cpp
@@ -1,4 +1,5 @@
 #include "Programs/PlayGround/PlayGroundExperiment.h"
+#include "Runtime/Concurrency/LockfreeContainer.h"
 #include "Runtime/Memory/MemoryModule.h"
 #include "Runtime/Misc/PMRContainers.h"
 #include "Runtime/Platform/PlatformAPIs.h"
@@ -12,7 +13,8 @@ namespace Omni
 {
     void ExperimentAll()
     {
-        Exp2ThreadsPingpong();
+        //Exp2ThreadsPingpong();
+        ExpLockfreeVSSingle();
     }
 
     struct CacheAlignedCounter
@@ -94,6 +96,68 @@ namespace Omni
 
         printf("\nExp2ThreadsPingpong(%d threads) duration:%fs\n", (int)nThreads, durtion.count());
     }
+
+    template<size_t TestCount>
+    struct ExpLockfreeStack
+    {
+    public:
+        ExpLockfreeStack(size_t)
+        {
+            LockfreeNode* n = LockfreeNodeCache::Alloc();
+            n->Data[0] = 0;
+            mStack.Push(n);
+        }
+        ~ExpLockfreeStack()
+        {
+            LockfreeNode* n = mStack.Pop();
+            LockfreeNodeCache::Free(n);
+        }
+        void Run(u64)
+        {
+            while (true)
+            {
+                auto n = mStack.Pop();
+                if (!n)
+                {
+                    PauseThread();
+                    continue;
+                }
+                size_t& r = *(size_t*)&(n->Data[0]);
+                ++r;
+                size_t x = r;
+                mStack.Push(n);
+                if (x >= TestCount)
+                    break;
+            }
+        }
+        static void RunStatic(ExpLockfreeStack* d, u64 i)
+        {
+            d->Run(i);
+        }
+    private:
+        LockfreeStack mStack;
+    };
+
     void ExpLockfreeVSSingle()
-    {}
+    {
+        constexpr size_t TestCount = 0x7ffffff;
+        size_t nThreads = std::thread::hardware_concurrency() / 4;
+        nThreads = 4;
+        ExpLockfreeStack<TestCount> test(nThreads);
+        PMRVector<std::thread> threads(MemoryModule::Get().GetPMRAllocator(MemoryKind::UserDefault));
+
+        auto tBegin = std::chrono::steady_clock::now();
+        for (size_t i = 0; i < nThreads; ++i)
+        {
+            threads.push_back(std::thread(&ExpLockfreeStack<TestCount>::RunStatic, &test, (u64)i));
+        }
+        for (size_t i = 0; i < nThreads; ++i)
+        {
+            threads[i].join();
+        }
+        auto tEnd = std::chrono::steady_clock::now();
+        std::chrono::duration<double> durtion = tEnd - tBegin;
+
+        printf("\nExpLockfreeVSSingle(%d threads) duration:%fs\n", (int)nThreads, durtion.count());
+    }
 }
